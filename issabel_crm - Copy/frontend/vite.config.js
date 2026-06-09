6286bb179461ef0694b7d671fedbe6ec597121d4
mnshub <mohammadmnsxbox@gmail.com>
2026-06-09 14:14:35 +0330

    Add files via upload

--- issabel_crm - Copy/frontend/vite.config.js
+++ issabel_crm - Copy/frontend/vite.config.js
@@ -1,1 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true // Crucial for instant Windows-to-Linux container file refresh tracking
+    }
+  }
+})
