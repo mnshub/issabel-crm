64f7d80d9c8eeebc8db23dd9873179434f3d1a0b
mnshub <mohammadmnsxbox@gmail.com>
2026-06-16 08:34:01 +0330

    test phase - wrapup form
    wrap up test on internal calls

--- issabel_crm - Copy/config/settings.py
+++ issabel_crm - Copy/config/settings.py
@@ -39,6 +39,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 
 # Application definition
 
